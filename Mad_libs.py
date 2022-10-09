179ed16287bb88db67150ab024fdb982c9bcceef
unknown <eldiny123@gmail.com>
2022-10-09 13:27:41 -0300

    first commit

---
+++ Mad_libs.py
@@ -0,0 +1,9 @@
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo 1: ")
+verbo2 = input("Verbo 2: ")
+sust = input("Sustantivo: ")
+
+mad_lib = f"Me {adj} programar! Estoy {verbo1} día a día, con mucha {verbo2}. Es {sust}, pero es entretenido!!"
+
+print(mad_lib)
---
+++ adivina_el_numero.py
@@ -0,0 +1,26 @@
+import random 
+
+
+def adivina_el_número(x):
+
+    print("==========================")
+    print(" ¡Bienvenido(a) al Juego! ")
+    print("==========================")
+    print("Tu meta es adivinar el número generado por la computadora.")
+
+    número_aleatorio = random.randint(1, x)
+
+    predicción = 0
+
+    while predicción != número_aleatorio:
+        predicción = int(input(f"Adivina un número entre 1 y {x}: "))
+
+        if predicción < número_aleatorio:
+            print("Intenta otra vez. Este número es muy pequeño.")
+        elif predicción > número_aleatorio:
+            print("Intenta otra vez. Esté número es muy grande.")
+
+    print(f"¡Felicitaciones! Adivinaste el número {número_aleatorio} correctamente.")
+
+
+adivina_el_número(10) 
